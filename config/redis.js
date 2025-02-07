579caaf83a072f67c4078116f2591d133d0e53e8
subashreetv <tvsubashree@gmail.com>
2025-02-07 22:32:55 +0530

    Update redis.js

--- config/redis.js
+++ config/redis.js
@@ -1,8 +1,13 @@
 import { createClient } from "redis";
 
-const client = createClient({
-    url: 'redis://redis:6379'
-    });
+let redis_config; 
+if(process.env.ENVIRONMENT == "production"){
+    redis_config = {  url: 'https://workable-toucan-11760.upstash.io',
+  token: '********'} 
+}else{
+    redis_config = { 'redis://redis:6379'}
+}
+const client = createClient(redis_config);
 
 client.on("error", (err) => console.log("Redis Error:", err));
 
