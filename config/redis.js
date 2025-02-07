e29a4c0219b96fa9412ff44f95c7c02269fdfdb0
subashreetv <tvsubashree@gmail.com>
2025-02-07 22:44:11 +0530

    Update redis.js

--- config/redis.js
+++ config/redis.js
@@ -2,8 +2,7 @@ import { createClient } from "redis";
 
 let redis_config; 
 if(process.env.ENVIRONMENT == "production"){
-    redis_config = {  url: process.env.REDIS_URL,
-  token: process.env.REDIS_PASS} 
+    redis_config = {  url: `rediss://default:${process.env.REDIS_PASS}@workable-toucan-11760.upstash.io:6379`} 
 }else{
     redis_config = { url: 'redis://redis:6379'}
 }
